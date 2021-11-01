03b1df761f4d8b617921c060b8a94e40b58a5a60
Abdul <AMalik@lcs.on.ca>
2021-11-01 09:09:53 -0400

    Added VStack title

---
+++ LayoutExercise/Views/POSTER 1.swift
@@ -0,0 +1,32 @@
+//
+//  POSTER 1.swift
+//  LayoutExercise
+//
+//  Created by Abdul Malik on 2021-10-27.
+//
+
+import SwiftUI
+
+struct POSTER_1: View {
+    var body: some View {
+        VStack {
+            
+            Text("OVO")
+                .fontWeight(.black)
+                .font(Font.custom("Arial", size: 64))
+            
+          
+                
+            
+                
+                
+            }
+        }
+    }
+
+
+struct POSTER_1_Previews: PreviewProvider {
+    static var previews: some View {
+        POSTER_1()
+    }
+}
